638e22a9ac6c294bfe7b3f56788f0eacefeb5d7f
mrlittlejns <77754353+mrlittlejns@users.noreply.github.com>
2021-01-21 00:05:25 +0300

    Add files via upload

---
+++ converbot.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=W0613, C0116
+# type: ignore[union-attr]
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+import numpy as np
+
+
+import logging
+from neuraltext import generate_text
+
+
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+GENDER, GENERATE, PHOTO, LOCATION, BIO = range(5)
+
+text = open('res.txt', 'rb').read().decode(encoding='utf-8')
+vocab = sorted(set(text))
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+text_as_int = np.array([char2idx[c] for c in text])
+
+
+def start(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [['Generate']]
+
+    text_to_send = generate_text(modelversion=1, temperature=0.47, start_string=u"Гарри Поттер")
+    update.message.reply_text(
+        f'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        f'Send /cancel to stop talking to me.\n\n'
+        f'{text_to_send}',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+        parse_mode='html'
+    )
+
+    return GENERATE
+
+
+def gender(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'I see! Please send me a photo of yourself, '
+        'so I know what you look like, or send /skip if you don\'t want to.',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+
+def generate(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    text_to_send = generate_text(modelversion=1, temperature=0.47, start_string=u"This is not a war ")
+    update.message.reply_text(
+        'This is your generated text: ', text_to_send
+        #reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return ConversationHandler.END
+
+
+def photo(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text(
+        'Gorgeous! Now, send me your location please, ' 'or send /skip if you don\'t want to.'
+    )
+
+    return LOCATION
+
+
+def skip_photo(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text(
+        'I bet you look great! Now, send me your location please, ' 'or send /skip.'
+    )
+
+    return LOCATION
+
+
+def location(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    update.message.reply_text(
+        'Maybe I can visit you sometime! ' 'At last, tell me something about yourself.'
+    )
+
+    return BIO
+
+
+def skip_location(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    update.message.reply_text(
+        'You seem a bit paranoid! ' 'At last, tell me something about yourself.'
+    )
+
+    return BIO
+
+
+def bio(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! I hope we can talk again some day.')
+
+    return ConversationHandler.END
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1421815649:AAEsU7g7lQbADFWLdsSmygTiQYOUbjBtxB0", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            # GENDER: [MessageHandler(Filters.regex('^(Generate|Not)$'), gender)],
+            GENERATE: [MessageHandler(Filters.regex('^(Generate|Not)$'), generate)],
+            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+            LOCATION: [
+                MessageHandler(Filters.location, location),
+                CommandHandler('skip', skip_location),
+            ],
+            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
